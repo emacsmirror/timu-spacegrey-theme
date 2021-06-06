4969d59df686e210f893935a2e44de61e2991df8
Aimé Bertrand <aime.bertrand@macowners.club>
2021-06-06 04:51:53 +0200

    Apply flycheck fixes.

--- timu-spacegrey-theme.el
+++ timu-spacegrey-theme.el
@@ -31,6 +31,9 @@
 ;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 ;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+;;; Commentary:
+;; Used Doom themes for boilerplate
+
 
 ;;; Code:
 
